56956055473d9f6a3f6fe31106c42746ad25322d
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-07-25 21:40:24 -0500

    draw and event listeners

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const canvas = document.querySelector("#drawing");
+const context = canvas.getContext("2d");
+let isDrawing = false;
+let pointX = 0;
+let pointY = 0;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.strokeStyle = "bada55";
+context.lineJoin = "round";
+context.lineCap = "round";
+
+function draw(event) {
+  if (!isDrawing) return;
+  context.beginPath();
+  context.moveTo(pointX, pointY);
+  context.lineTo(event.offsetX, event.offsetY);
+  context.stroke();
+  [pointX, pointY] = [event.offsetX, event.offsetY];
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
